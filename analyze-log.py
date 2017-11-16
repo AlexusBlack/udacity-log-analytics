ee52560a8ca5f6c58bcf4302e1a645ab6c429285
Alex Chernov <alexusblack@gmail.com>
2017-11-16 21:25:57 +1100

    better code organisation

--- analyze-log.py
+++ analyze-log.py
@@ -4,20 +4,23 @@
 import psycopg2
 import reports
 
-print("Starting log analysis")
-print("Requesting data")
+def main():
+    print("Starting log analysis")
+    print("Requesting data")
 
-# Configuring connection
-db = psycopg2.connect("dbname=news")
-c = db.cursor()
+    # Configuring connection
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
 
-# What are the most popular three articles of all time?
-reports.get_top3_articles(c)
+    # What are the most popular three articles of all time?
+    reports.get_top3_articles(c)
 
-# Who are the most popular article authors of all time?
+    # Who are the most popular article authors of all time?
 
-# Which authors wrote most articles?
+    # Which authors wrote most articles?
 
-# Which authors has best rate articles to views?
+    # Which authors has best rate articles to views?
 
-# On which days did more than 1% of requests lead to errors?
+    # On which days did more than 1% of requests lead to errors?
+
+main()
